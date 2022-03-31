11b7e34d0f53967f2fe1387e9c258e3b2f549542
MaslowskiLukasz <l.maslowski2012@gmail.com>
2022-04-01 01:17:20 +0200

    Recreate middle part with JS inside separate moodule - home.js

--- src/home.js
+++ src/home.js
@@ -13,6 +13,52 @@ function createHeadingContent() {
   return heading;
 }
 
+function createMiddleContent() {
+  const menuSection = document.createElement('section');
+  const menu = document.createElement('div');
+  const title = document.createElement('h2');
+  const btnRow = document.createElement('div');
+  const btn = document.createElement('button');
+  const burgersNames = ['Kura', 'Bekon', 'Mięśniak', 'Nachos'];
+  
+  menuSection.id = 'menu-section';
+  menu.id = 'menu';
+  btn.id = 'full-menu-btn';
+  btn.textContent = 'See full menu'
+  title.textContent = 'Menu';
+  btnRow.classList.add('btn-row');
+
+  for(let i = 0; i < burgersNames.length; i++) {
+    const item = document.createElement('div');
+    const name = document.createElement('h3');
+    const price = document.createElement('p');
+    
+
+    item.classList.add('menu-item');
+    name.textContent = burgersNames[i];
+    price.classList.add('price');
+    price.textContent = '23.50 zł';
+
+    item.appendChild(name);
+    item.appendChild(price);
+
+    menu.appendChild(item);
+  }
+
+  btnRow.appendChild(btn);
+  menuSection.appendChild(title);
+  menuSection.appendChild(menu);
+  menuSection.appendChild(btnRow);
+
+  return menuSection;
+}
+
+function createBottomContent() {
+  const tmp = document.createElement('h2');
+  tmp.textContent = 'test';
+  return tmp;
+}
+
 let openMainPage = function() {
   const content = document.getElementById('content');
   const top = document.createElement('section');
@@ -23,7 +69,11 @@ let openMainPage = function() {
   middle.id = 'middle';
   bottom.id = 'bottom';
   top.appendChild(createHeadingContent());
+  middle.appendChild(createMiddleContent());
+  bottom.appendChild(createBottomContent());
   content.appendChild(top);
+  content.appendChild(middle);
+  content.appendChild(bottom);
 }
 
 export { openMainPage };
