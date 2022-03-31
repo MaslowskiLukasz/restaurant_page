651da86ffd26f228d9d69285c64a587f195917d7
MaslowskiLukasz <l.maslowski2012@gmail.com>
2022-04-01 01:35:22 +0200

    Recreate bottom part with JS inside separate module - home.js

--- src/home.js
+++ src/home.js
@@ -53,10 +53,42 @@ function createMiddleContent() {
   return menuSection;
 }
 
+function createInfo(x) {
+  const contactInfo = document.createElement('div');
+  const streets = ['Ul. Pierwszy Adres 3', 'Ul. Drugi Adres 42/1'];
+  const phones = ['123 456 678', '987 654 321'];
+  const opening = ['Pn-Nd: 12:00 - 22:00', 'Pn-Nd: 12:00 - 21:00'];
+  
+  const street = document.createElement('h3');
+  const phone = document.createElement('p');
+  const open = document.createElement('p');
+  
+  street.textContent = streets[x];
+  phone.textContent = phones[x];
+  open.textContent = opening[x];
+
+  contactInfo.classList.add('contact-info');
+  contactInfo.appendChild(street);
+  contactInfo.appendChild(phone);
+  contactInfo.appendChild(open);
+
+  return contactInfo;
+}
+
 function createBottomContent() {
-  const tmp = document.createElement('h2');
-  tmp.textContent = 'test';
-  return tmp;
+  const content = document.createElement('div');
+  const title = document.createElement('h2');
+
+  title.textContent = 'Contact';
+
+  content.appendChild(title);
+  
+  for(let i = 0; i < 2; i++) {
+    content.appendChild(createInfo(i));
+  }
+
+  return content;
+
 }
 
 let openMainPage = function() {
@@ -68,9 +100,11 @@ let openMainPage = function() {
   top.id = 'top';
   middle.id = 'middle';
   bottom.id = 'bottom';
+
   top.appendChild(createHeadingContent());
   middle.appendChild(createMiddleContent());
   bottom.appendChild(createBottomContent());
+
   content.appendChild(top);
   content.appendChild(middle);
   content.appendChild(bottom);
